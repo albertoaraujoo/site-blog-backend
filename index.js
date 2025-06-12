991a8ff73cc2c42a7f2849fe09643db580a0e98b
Alberto Araújo <alberto.araujoo@hotmail.com>
2025-06-12 16:34:43 -0300

    feat: all app methods added, post, put, delete..

--- index.js
+++ index.js
@@ -10,8 +10,23 @@ app.listen(3001, () => {
   console.log("Server running on http://localhost:3001");
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello from node API (changes)");
+app.get("/blog/posts", async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/blog/post/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findById(id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 app.post("/blog/posts", async (req, res) => {
@@ -21,9 +36,46 @@ app.post("/blog/posts", async (req, res) => {
       message: "Post created successfully",
       post: post,
     });
-  } catch (err) {
+  } catch (error) {
     console.error("Error in POST /blog/posts:", err);
-    res.status(500).send("Internal Server Error");
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//update a post
+app.put("/blog/post/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      { new: true }
+    );
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    const updatedPost = await Post.findById(id);
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//delete a post
+app.delete("/blog/post/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findByIdAndDelete(id);
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    res.status(200).json({ message: "Post deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
@@ -31,7 +83,7 @@ mongoose
   .connect(
     "mongodb+srv://albertoaraujoo:dYh1BcweiCxAslxt@postsdb.mhngbit.mongodb.net/?retryWrites=true&w=majority&appName=PostsDB"
   )
-  .then((result) => {
+  .then(() => {
     console.log("Connected to MongoDB successfully");
   })
   .catch((err) => {
